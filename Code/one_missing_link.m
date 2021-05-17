33b75b4d48870b24b496b390e8b5e18b1e2d290a
2019JTM2677 <55458057+2019JTM2677@users.noreply.github.com>
2021-05-17 12:00:02 +0530

    Add files via upload

--- Code/one_missing_link.m
+++ Code/one_missing_link.m
@@ -1,9 +1,9 @@
 function [opt,src_conn_id,dest_conn_id,nodes_in_path] = one_missing_link(x_n_sq,n,h,src_id,dest_id,B,C,method)
     
-    opt=0;%r_id=[];c_id=[];
+    opt=0;
     dest_conn_id=[];
     src_conn_id=[];
-    nodes_in_path=[];    %EE = size(x_n_sq);
+    nodes_in_path=[];    
     
     A = diag(x_n_sq);
     Z = B*A*C;
@@ -11,18 +11,18 @@ function [opt,src_conn_id,dest_conn_id,nodes_in_path] = one_missing_link(x_n_sq,
     
     if any(sum(Z)>1) || any(sum(Z,2)>1)        % More than one non zero in any column of Z,i.e. more than 1 outgoing edges from same node
         %fprintf("invalid path no need for further check")
-        %fprintf("BUG")
-        
     else
-    
         dest_mat=[]; 
         src_mat=[];
+        
+        % Set parameter for missing-link
         switch lower(method)
             case 'se'
                 param=h-1;
             case 'de'
                 param=h-2;
         end
+        
         for nd=1:param
             Za = Z^nd;
             dest_mat=[dest_mat;Za(end,:)];
@@ -33,15 +33,14 @@ function [opt,src_conn_id,dest_conn_id,nodes_in_path] = one_missing_link(x_n_sq,
         
         r = zeros(1,param);
         r(1)=1;
-        %display(r);
         c = zeros(param,1);
         c(1)=1;
         rk=0;ck=0;
-        %r_id=[];c_id=[];
-        %nodes_in_path=[]; % all nodes appeared in path
+        
         nodes_in_path = union(nodes_in_path,[src_id,dest_id]);
         src_flag=0;
         dest_flag=0;
+        
         for k=1:param
             if k==1 || src_flag==1
                 I=ismember(src_mat,r,'rows');
@@ -49,7 +48,7 @@ function [opt,src_conn_id,dest_conn_id,nodes_in_path] = one_missing_link(x_n_sq,
                 if  sum(I)==1 
                     [~,r_id]=ismember(r,src_mat,'rows'); % node connected to src
                     nodes_in_path = union(nodes_in_path,r_id);
-                    if r_id~=src_id && r_id ~= dest_id  % node connected to src in h-1 hops can't be src or dest
+                    if r_id~=src_id && r_id ~= dest_id   % node connected to src in h-1 hops can't be src or dest
                         src_conn_id = r_id;
                         src_flag=1;
                         rk=rk+1;
@@ -59,8 +58,7 @@ function [opt,src_conn_id,dest_conn_id,nodes_in_path] = one_missing_link(x_n_sq,
                     end
                 else
                     src_flag=0;
-                % If no match found in first iteration then we don't need to go for next iteration as if hop len 1 doesnt exist 2,3 can't exist
-                % similarily if 2 doesn't exist 3  and onwards can't
+               
                 end
             end
 
@@ -81,28 +79,23 @@ function [opt,src_conn_id,dest_conn_id,nodes_in_path] = one_missing_link(x_n_sq,
                     dest_flag=0;
                 end
             end
-
-
         end
+        
         if isempty(src_conn_id)
             src_conn_id = src_id;
         end
+        
         if isempty(dest_conn_id)
             dest_conn_id = dest_id;
         end
 
-
         switch lower(method)
         case 'se'
             if rk+ck==param && src_conn_id~=dest_conn_id
-                %display(r)
-                %display(c)
                 opt=1;
             end
         case 'de'
-            if rk+ck==param
-                %display(rk)
-                %display(ck)
+            if rk+ck==param && src_conn_id~=dest_conn_id
                 opt=1;
             end
         end
